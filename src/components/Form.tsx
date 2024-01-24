9e8ff897177dabe7034fc5f96764f5a4e2f6dc9d
João Pedro Bessa <joaopbessa1998@gmail.com>
2024-01-24 18:31:42 +0000

    feat: types added to form, states created and handleSubmit function created

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,20 @@
+import { useState, FormEvent } from "react";
+import { User } from "../types/User";
+
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agree, setAgree] = useState(false);
+
+  const [errors, setErrors] = useState<User | null>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    alert("Test");
+  };
+
   return (
-    <form className="flex flex-col gap-3 ">
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="name">
           Name & Surname
