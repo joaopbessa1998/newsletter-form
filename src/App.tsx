cb475b58280bbdc3548baf98bf5d9a6b6a3de272
João Pedro Bessa <joaopbessa1998@gmail.com>
2024-01-24 18:24:44 +0000

    feat: styles added to homepage and form component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-red-600">Sign up</h1>
-      <p>Subscribe to our newsletter and stay informed</p>
-      <div>
+    <div className="bg-slate-900 min-h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="font-bold text-[2rem] text-white">Sign up</h1>
+      <p className="text-white">
+        Subscribe to our newsletter and stay informed
+      </p>
+      <div className="w-96 mt-4 bg-stone-200 px-4 py-5 rounded-lg">
         <Form />
       </div>
-      <p>
+      <p className="text-slate-100 text-xs w-96 mt-2 text-center">
         By signing up you will receive our emails with the best tips, news and
         offers.
       </p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,43 @@
 const Form = () => {
   return (
-    <form>
-      <div>
-        <label>Name & Surname</label>
-        <input type="text" placeholder="John Doe" />
+    <form className="flex flex-col gap-3 ">
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">
+          Name & Surname
+        </label>
+        <input
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+          type="text"
+          placeholder="John Doe"
+        />
       </div>
-      <div>
-        <label>Email</label>
-        <input type="email" placeholder="johndoe@example.com" />
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+          type="email"
+          placeholder="johndoe@example.com"
+        />
       </div>
-      <div>
-        <a href="#">Read our terms</a>
-        <div>
+      <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2">
+          Read our terms
+        </a>
+        <div className="flex gap-2 items-center">
           <input type="checkbox" />
-          <label>I've read and agree</label>
+          <label className="text-sm" htmlFor="agree">
+            I've read and agree
+          </label>
         </div>
       </div>
-      <button>Subscribe</button>
+      <button
+        type="submit"
+        className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white"
+      >
+        Subscribe
+      </button>
     </form>
   );
 };
